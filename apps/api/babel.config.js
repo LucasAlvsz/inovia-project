272a2e96c84ee44bf192d080af66c13f48914d5f
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2023-01-09 07:06:15 -0300

    feat: api structure

---
+++ apps/api/babel.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+	presets: [
+		[
+			"@babel/preset-env",
+			{
+				targets: {
+					node: "current",
+				},
+			},
+		],
+		"@babel/preset-typescript",
+	],
+	plugins: [
+		[
+			"module-resolver",
+			{
+				alias: {
+					"@": "./src",
+					"@utils": "./src/utils",
+					"@types": "./src/types",
+					"@config": "./src/config",
+					"@errors": "./src/errors",
+					"@routes": "./src/routes",
+					"@schemas": "./src/schemas",
+					"@services": "./src/services",
+					"@controllers": "./src/controllers",
+					"@middlewares": "./src/middlewares",
+					"@repositories": "./src/repositories",
+				},
+			},
+		],
+	],
+	ignore: ["**/*.spec.ts"],
+}
---
+++ apps/api/src/errors/index.ts
@@ -0,0 +1,9 @@
+class AppError {
+	public readonly code: number
+	public readonly message: string
+
+	constructor(code: number, message: string) {
+		this.code = code
+		this.message = message
+	}
+}
